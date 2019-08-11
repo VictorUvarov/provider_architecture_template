a46d71876c585d7428d96b50b8b0e192eec2e01c
Victor Uvarov <victoruvarov23@gmail.com>
2019-08-11 12:04:53 -0700

    fix format script

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "#!/bin/sh\nexec flutter format app/lib/ app/test/" >> ../.git/hooks/pre-commit
-chmod +x ../.git/hooks/pre-commit
+echo "#!/bin/sh\nexec flutter format lib/ test/" >> .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
