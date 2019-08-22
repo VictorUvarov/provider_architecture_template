061f6fbd244994481d8aabf2dfa6277660d4cf6e
Victor Uvarov <victoruvarov23@gmail.com>
2019-08-21 19:04:59 -0700

    remove unused routes

--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider_start/core/constant/route_paths.dart';
 import 'package:provider_start/core/enums/tab_view.dart';
 import 'package:provider_start/core/managers/dialog_manager.dart';
-import 'package:provider_start/ui/views/home/home_view.dart';
-import 'package:provider_start/ui/views/settings/settings_view.dart';
 import 'package:provider_start/ui/views/tab_container.dart';
 
 class Router {
@@ -15,12 +13,6 @@ class Router {
         var view = settings.arguments as TabView;
         return MaterialPageRoute(
             builder: (_) => DialogManager(child: TabContainer(view: view)));
-      case RoutePaths.Home:
-        return MaterialPageRoute(
-            builder: (_) => DialogManager(child: HomeView()));
-      case RoutePaths.Settings:
-        return MaterialPageRoute(
-            builder: (_) => DialogManager(child: SettingsView()));
 
       default:
         return MaterialPageRoute(
