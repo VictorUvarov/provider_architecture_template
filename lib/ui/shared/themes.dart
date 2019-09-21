6bf16b098f2d36ce953b7ff8dd14ecc269da89a7
Victor Uvarov <victoruvarov23@gmail.com>
2019-09-21 12:12:46 -0700

    fix iOS darkTheme

--- lib/ui/shared/themes.dart
+++ lib/ui/shared/themes.dart
@@ -21,9 +21,30 @@ final darkMaterialTheme = ThemeData.dark()
 
 final primaryCupertinoTheme = CupertinoThemeData(
   primaryColor: Colors.purple,
+  brightness: Brightness.light,
+  textTheme: CupertinoTextThemeData(
+    primaryColor: Colors.white,
+    navTitleTextStyle: TextStyle(
+      color: Colors.purple,
+    ),
+    tabLabelTextStyle: TextStyle(
+      color: Colors.purple,
+    ),
+  ),
 );
 
 final darkCupertinoTheme = CupertinoThemeData(
-  primaryColor: Colors.black,
+  primaryColor: Colors.blue,
   scaffoldBackgroundColor: Colors.black87,
+  barBackgroundColor: Colors.black12,
+  brightness: Brightness.dark,
+  textTheme: CupertinoTextThemeData(
+    primaryColor: Colors.white,
+    navTitleTextStyle: TextStyle(
+      color: Colors.blue,
+    ),
+    tabLabelTextStyle: TextStyle(
+      color: Colors.blue,
+    ),
+  ),
 );
