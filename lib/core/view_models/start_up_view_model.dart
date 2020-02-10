a7f4c8acbef161c7ce3d9d9825e948913b728c21
Victor Uvarov <victoruvarov23@gmail.com>
2020-02-09 22:53:15 -0800

    initialize services in once

--- lib/core/view_models/start_up_view_model.dart
+++ lib/core/view_models/start_up_view_model.dart
@@ -1,9 +1,6 @@
 import 'package:pedantic/pedantic.dart';
 import 'package:provider_start/core/constant/view_routes.dart';
-import 'package:provider_start/core/data_sources/posts/posts_local_data_source.dart';
-import 'package:provider_start/core/data_sources/users/users_local_data_source.dart';
 import 'package:provider_start/core/services/auth/auth_service.dart';
-import 'package:provider_start/core/services/hardware_info/hardware_info_service.dart';
 import 'package:provider_start/core/services/navigation/navigation_service.dart';
 import 'package:provider_start/core/view_models/base_view_model.dart';
 import 'package:provider_start/locator.dart';
@@ -11,19 +8,11 @@ import 'package:provider_start/locator.dart';
 class StartUpViewModel extends BaseViewModel {
   final _authService = locator<AuthService>();
   final _navigationService = locator<NavigationService>();
-  final _hardwareInfoService = locator<HardwareInfoService>();
-  final _postsLocalDataSource = locator<PostsLocalDataSource>();
-  final _usersLocalDataSource = locator<UsersLocalDataSource>();
 
   Future handleStartUpLogic() async {
     final hasLoggedInUser = await _authService.isUserLoggedIn();
 
     if (hasLoggedInUser) {
-      await Future.wait([
-        _hardwareInfoService.init(),
-        _postsLocalDataSource.init(),
-        _usersLocalDataSource.init(),
-      ]);
       unawaited(_navigationService.pushReplacementNamed(ViewRoutes.main));
     } else {
       unawaited(_navigationService.pushReplacementNamed(ViewRoutes.login));
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:provider_start/core/data_sources/posts/posts_local_data_source.dart';
+import 'package:provider_start/core/data_sources/users/users_local_data_source.dart';
 import 'package:provider_start/core/localization/localization.dart';
 import 'package:provider_start/core/managers/core_manager.dart';
 import 'package:provider_start/core/managers/dialog_manager.dart';
 import 'package:provider_start/core/services/dialog/dialog_service.dart';
+import 'package:provider_start/core/services/hardware_info/hardware_info_service.dart';
 import 'package:provider_start/core/services/navigation/navigation_service.dart';
 import 'package:provider_start/core/utils/logger.dart';
 import 'package:provider_start/locator.dart';
@@ -19,6 +22,17 @@ void main() async {
 
   setupLogger();
   await setupLocator();
+
+  final postsLocalDataSource = locator<PostsLocalDataSource>();
+  final usersLocalDataSource = locator<UsersLocalDataSource>();
+  final hardwareInfoService = locator<HardwareInfoService>();
+
+  await Future.wait([
+    postsLocalDataSource.init(),
+    usersLocalDataSource.init(),
+    hardwareInfoService.init(),
+  ]);
+
   runApp(MyApp());
 }
 
