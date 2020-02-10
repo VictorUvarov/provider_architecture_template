aed79e297f20676a1cc8f714bb9b3eb24ec0e389
Victor Uvarov <victoruvarov23@gmail.com>
2020-02-09 21:32:28 -0800

    don't spawn isolate for one object

--- lib/core/data_sources/users/users_remote_data_source.dart
+++ lib/core/data_sources/users/users_remote_data_source.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:provider_start/core/constant/api_routes.dart';
 import 'package:provider_start/core/models/user/user.dart';
 import 'package:provider_start/core/services/http/http_service.dart';
-import 'package:provider_start/core/services/http/http_service_impl.dart';
-import 'package:provider_start/core/utils/logger.dart';
 import 'package:provider_start/locator.dart';
 
 abstract class UsersRemoteDataSource {
@@ -15,23 +12,9 @@ class UsersRemoteDataSourceImpl implements UsersRemoteDataSource {
 
   @override
   Future<User> fetchUser(int uid) async {
-    final user = await compute(_fetchUserRemotely, uid);
-    return user;
-  }
+    final postsMap = await httpService.getHttp('${ApiRoutes.users}/$uid')
+        as Map<String, dynamic>;
 
-  static Future<User> _fetchUserRemotely(int userId) async {
-    setupLogger();
-
-    final httpService = HttpServiceImpl();
-
-    try {
-      final postsMap = await httpService.getHttp('${ApiRoutes.users}/$userId')
-          as Map<String, dynamic>;
-
-      final user = User.fromMap(postsMap);
-      return user;
-    } finally {
-      httpService.dispose();
-    }
+    return User.fromMap(postsMap);
   }
 }
