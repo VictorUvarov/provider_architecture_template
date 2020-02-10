4e374e141f005c5b8c9b5da476199c1c3e1b8f80
Victor Uvarov <victoruvarov23@gmail.com>
2020-02-09 22:02:20 -0800

    add posts remote data source tests

--- lib/core/data_sources/posts/posts_remote_data_source.dart
+++ lib/core/data_sources/posts/posts_remote_data_source.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:provider_start/core/constant/api_routes.dart';
 import 'package:provider_start/core/models/post/post.dart';
 import 'package:provider_start/core/services/http/http_service.dart';
-import 'package:provider_start/core/utils/logger.dart';
 import 'package:provider_start/locator.dart';
 
 abstract class PostsRemoteDataSource {
@@ -14,25 +12,12 @@ class PostsRemoteDataSourceImpl implements PostsRemoteDataSource {
 
   @override
   Future<List<Post>> fetchPosts() async {
-    final posts = await compute(_fetchPosts, httpService);
+    final postsJsonData =
+        await httpService.getHttp(ApiRoutes.posts) as List<dynamic>;
+
+    final List<Post> posts =
+        postsJsonData.map<Post>((postMap) => Post.fromMap(postMap)).toList();
+
     return posts;
-  }
-
-  static Future<List<Post>> _fetchPosts(HttpService httpService) async {
-    setupLogger();
-
-    try {
-      final postsJsonData =
-          await httpService.getHttp(ApiRoutes.posts) as List<dynamic>;
-
-      final posts = postsJsonData
-          .map((data) => data as Map<String, dynamic>)
-          .map((postMap) => Post.fromMap(postMap))
-          .toList();
-
-      return posts;
-    } finally {
-      httpService.dispose();
-    }
   }
 }
---
+++ test/core/data_sources/posts/posts_remote_data_source_test.dart
@@ -0,0 +1,71 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider_start/core/constant/api_routes.dart';
+import 'package:provider_start/core/data_sources/posts/posts_remote_data_source.dart';
+import 'package:provider_start/core/models/post/post.dart';
+import 'package:provider_start/core/services/http/http_service.dart';
+import 'package:provider_start/locator.dart';
+
+class MockHttpService extends Mock implements HttpService {}
+
+void main() {
+  PostsRemoteDataSource postsRemoteDataSource;
+  HttpService httpService;
+
+  final postsJsonData = [
+    {"userId": 1, "id": 1, "title": "Post 1", "body": "Post 1 body"},
+    {"userId": 1, "id": 2, "title": "Post 2", "body": "Post 2 body"},
+  ];
+
+  final mockPost1 = Post(
+    (p) => p
+      ..id = 1
+      ..title = 'title'
+      ..description = 'desc'
+      ..userId = 1,
+  );
+  final mockPost2 = mockPost1.rebuild(
+    (p) => p
+      ..id = 2
+      ..userId = 2,
+  );
+  final mockPosts = [mockPost1, mockPost2];
+
+  setUp(() async {
+    await setupLocator(test: true);
+    locator.allowReassignment = true;
+
+    locator.registerSingleton<HttpService>(MockHttpService());
+    httpService = locator<HttpService>();
+
+    postsRemoteDataSource = locator<PostsRemoteDataSource>();
+  });
+
+  test('data source should call httpService.getHttp when we fetch posts',
+      () async {
+    // arrange
+    when(httpService.getHttp(ApiRoutes.posts))
+        .thenAnswer((_) => Future.value(postsJsonData));
+
+    // act
+    await postsRemoteDataSource.fetchPosts();
+
+    // assert
+    verify(httpService.getHttp(ApiRoutes.posts));
+  });
+
+  test('data source should convert json data to posts when we fetch posts',
+      () async {
+    // arrange
+    when(httpService.getHttp(ApiRoutes.posts))
+        .thenAnswer((_) => Future.value(postsJsonData));
+    final mappedPosts =
+        postsJsonData.map<Post>((postMap) => Post.fromMap(postMap)).toList();
+
+    // act
+    final posts = await postsRemoteDataSource.fetchPosts();
+
+    // assert
+    expect(posts, equals(mappedPosts));
+  });
+}
