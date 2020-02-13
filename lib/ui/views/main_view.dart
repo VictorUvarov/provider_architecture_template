387a3d7ded48dacb46a89e52f4b62092fed6b63a
Victor Uvarov <victoruvarov23@gmail.com>
2020-02-12 18:48:58 -0800

    Add lazy index stack for main views
    - Normal indexed stack would load all the main views at the same time,
    instead lazily load the widgets in the stack

--- lib/ui/views/main_view.dart
+++ lib/ui/views/main_view.dart
@@ -6,11 +6,17 @@ import 'package:provider_start/core/localization/localization.dart';
 import 'package:provider_start/core/view_models/main_view_model.dart';
 import 'package:provider_start/ui/views/home_view.dart';
 import 'package:provider_start/ui/views/settings_view.dart';
+import 'package:provider_start/ui/widgets/lazy_index_stack.dart';
 
 /// Main view container that handles rendering pages according to which bottom
 /// navigation bar item is tapped
 ///   - can be replaced with a [TabView]
 class MainView extends StatelessWidget {
+  final _views = [
+    HomeView(),
+    SettingsView(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     final local = AppLocalizations.of(context);
@@ -18,12 +24,11 @@ class MainView extends StatelessWidget {
     return ViewModelProvider<MainViewModel>.withConsumer(
       viewModel: MainViewModel(),
       builder: (context, model, child) => PlatformScaffold(
-        body: IndexedStack(
+        body: LazyIndexedStack(
+          reuse: true,
           index: model.index,
-          children: <Widget>[
-            HomeView(),
-            SettingsView(),
-          ],
+          itemCount: _views.length,
+          itemBuilder: (_, index) => _views[index],
         ),
         bottomNavBar: PlatformNavBar(
           currentIndex: model.index,
---
+++ lib/ui/widgets/lazy_index_stack.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/widgets.dart';
+
+class LazyIndexedStack extends StatefulWidget {
+  final AlignmentGeometry alignment;
+  final TextDirection textDirection;
+  final StackFit sizing;
+
+  /// Current index of the stack
+  final int index;
+
+  /// number of views
+  final int itemCount;
+
+  /// Builder for the current index
+  final IndexedWidgetBuilder itemBuilder;
+
+  /// Reuse the created view
+  final bool reuse;
+
+  LazyIndexedStack({
+    Key key,
+    this.alignment = AlignmentDirectional.topStart,
+    this.textDirection,
+    this.sizing = StackFit.loose,
+    this.index,
+    this.reuse = true,
+    @required this.itemBuilder,
+    this.itemCount = 0,
+  }) : super(key: key);
+
+  @override
+  _LazyIndexedStackState createState() => _LazyIndexedStackState();
+}
+
+class _LazyIndexedStackState extends State<LazyIndexedStack> {
+  List<Widget> _children;
+  List<bool> _loaded;
+
+  @override
+  void initState() {
+    _loaded = [];
+    _children = [];
+    for (int i = 0; i < widget.itemCount; ++i) {
+      if (i == widget.index) {
+        _children.add(widget.itemBuilder(context, i));
+        _loaded.add(true);
+      } else {
+        _children.add(Container());
+        _loaded.add(false);
+      }
+    }
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(LazyIndexedStack oldWidget) {
+    for (int i = 0; i < widget.itemCount; ++i) {
+      if (i == widget.index) {
+        if (!_loaded[i]) {
+          _children[i] = widget.itemBuilder(context, i);
+          _loaded[i] = true;
+        } else {
+          if (widget.reuse) {
+            return;
+          }
+          _children[i] = widget.itemBuilder(context, i);
+        }
+      }
+    }
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IndexedStack(
+      index: widget.index,
+      alignment: widget.alignment,
+      textDirection: widget.textDirection,
+      sizing: widget.sizing,
+      children: _children,
+    );
+  }
+}
