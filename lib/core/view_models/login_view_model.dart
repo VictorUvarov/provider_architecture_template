64cc8e2d6f558d730ec09139937b49736aad93f4
Fernando Ferrara <ferrarafer@gmail.com>
2020-03-07 16:11:35 -0300

    Remove the logger

--- lib/core/view_models/login_view_model.dart
+++ lib/core/view_models/login_view_model.dart
@@ -5,19 +5,16 @@ import 'package:provider_start/core/exceptions/auth_exception.dart';
 import 'package:provider_start/core/mixins/validators.dart';
 import 'package:provider_start/core/services/auth/auth_service.dart';
 import 'package:provider_start/core/services/navigation/navigation_service.dart';
-import 'package:provider_start/core/utils/simple_log_printer.dart';
 import 'package:provider_start/core/view_models/base_view_model.dart';
 import 'package:provider_start/locator.dart';
 
 class LoginViewModel extends BaseViewModel with Validators {
-  final _log = getLogger();
   final _authService = locator<AuthService>();
   final _navigationService = locator<NavigationService>();
 
   bool get busy => state == ViewState.Busy;
 
   Future<void> login(String email, String password) async {
-    _log.d('email:$email password:$password');
     setState(ViewState.Busy);
 
     try {
