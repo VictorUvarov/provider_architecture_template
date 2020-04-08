8f282a8a85ec72e536d4d2b165237664bbe809c2
Edward Prokopik <ejprok@gmail.com>
2020-04-07 19:14:16 -0700

    Add StateResponsive widget for easily setting up ViewState dependent UI

---
+++ lib/ui/widgets/state_responsive.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider_start/core/enums/view_state.dart';
+
+class StateResponsive extends StatelessWidget {
+  final ViewState state;
+  final Widget idleWidget;
+  final Widget busyWidget;
+  final Widget dataFetchedWidget;
+  final Widget noDataAvailableWidget;
+  final Widget errorWidget;
+  final Widget successWidget;
+  final Widget waitingForInputWidget;
+
+  // returns a widget based on the provided ViewState
+  const StateResponsive({
+    Key key,
+    @required this.state,
+    @required this.idleWidget,
+    this.busyWidget,
+    this.errorWidget,
+    this.dataFetchedWidget,
+    this.noDataAvailableWidget,
+    this.successWidget,
+    this.waitingForInputWidget,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    switch (state) {
+      case ViewState.Idle:
+        return idleWidget;
+      case ViewState.Busy:
+        return busyWidget ?? idleWidget;
+      case ViewState.DataFetched:
+        return dataFetchedWidget ?? idleWidget;
+      case ViewState.NoDataAvailable:
+        return noDataAvailableWidget ?? idleWidget;
+      case ViewState.Error:
+        return errorWidget ?? idleWidget;
+      case ViewState.Success:
+        return successWidget ?? idleWidget;
+      default:
+        return idleWidget;
+    }
+  }
+}
