758773a00a41255ee479edbcf8fd7a31908fadd3
Victor Uvarov <victoruvarov23@gmail.com>
2020-05-28 15:18:38 -0700

    hide posts from view
    - remove stored posts

--- lib/ui/views/home/home_view_model.dart
+++ lib/ui/views/home/home_view_model.dart
@@ -1,5 +1,4 @@
 import 'package:provider_start/core/exceptions/repository_exception.dart';
-import 'package:provider_start/core/models/post/post.dart';
 import 'package:provider_start/core/repositories/posts_repository/posts_repository.dart';
 import 'package:provider_start/locator.dart';
 import 'package:provider_start/state/base_view_model.dart';
@@ -8,9 +7,6 @@ import 'package:provider_start/ui/views/home/home_view_state.dart';
 class HomeViewModel extends BaseViewModel<HomeViewState> {
   final _postsRepository = locator<PostsRepository>();
 
-  List<Post> _posts = [];
-  List<Post> get posts => _posts;
-
   Future<void> init() async {
     await _attemptFetchPosts();
   }
@@ -18,17 +14,12 @@ class HomeViewModel extends BaseViewModel<HomeViewState> {
   Future<void> _attemptFetchPosts() async {
     setState(HomeViewState.loading());
     try {
-      final fetchedPosts = await _postsRepository.fetchPosts();
-      _posts = fetchedPosts.take(20).toList();
-      _checkIfAvailableData();
+      final posts = await _postsRepository.fetchPosts();
+      posts.isEmpty
+          ? setState(HomeViewState.noPosts())
+          : setState(HomeViewState.loaded(posts));
     } on RepositoryException catch (e) {
       setState(HomeViewState.error(e.message));
     }
-  }
-
-  void _checkIfAvailableData() {
-    (_posts.isEmpty)
-        ? setState(HomeViewState.noPosts())
-        : setState(HomeViewState.loaded(_posts));
   }
 }
